22a06550072a32a4339da3c06f25e3c733d4754f
Bingsu <ks2515@naver.com>
2022-01-07 17:41:09 +0900

    fix: 문제 번호 int순으로 정렬되게 수정, st.cache 사용

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from pathlib import Path
 import streamlit as st
 
-root = Path(".")
 
-path_to_name = {p: p.name.removesuffix(".py") for p in root.glob("problem/**/*.py")}
-options = sorted(path_to_name.keys())
+@st.cache
+def get_options() -> tuple[dict[Path, str], list[Path]]:
+    root = Path(".")
+    path_to_name = {p: p.name.removesuffix(".py") for p in root.glob("problem/**/*.py")}
+    options = sorted(path_to_name.keys(), key=lambda k: int(path_to_name[k].split()[0]))
+    return path_to_name, options
+
+
+path_to_name, options = get_options()
 
 with st.sidebar:
     st.image("sundae.png")
